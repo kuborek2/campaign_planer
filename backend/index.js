a5b3405e814260ef7c3ecd29b370bd77e00b715e
Jakub Krezolek <kuborek2@gmail.com>
2022-07-19 19:11:41 +0200

    Cleaned up backend from exercise i took it from by a small margin

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,10 @@
 const http = require('http');
 const express = require('express');
-const questions = require('./questions').questions;
 const config = require('./config').config;
-const Routes = require('./REST/routes');
+const Routes = require('./routes');
 const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json({limit: '2048kb'}));
-
-app.get('/api/questions', (request, response) => {
-   response.send(questions);
-   console.log("udalo sie");
-});
 
 let routes = new Routes();
 routes.routes(app);
