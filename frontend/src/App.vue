ee49a6ee396f58feef5020fa4c9d10c40e225142
Jakub Krezolek <kuborek2@gmail.com>
2022-07-21 16:22:17 +0200

    Small changes on frontend opposite to default state

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <Header />
+  </header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,16 +17,16 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
-  </header>
+    </div> -->
 
-  <RouterView />
+
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    width: 100%;
+    height: 150px;
 }
 
 .logo {
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div class="container">
+        <h3 class="title">
+            <RouterLink to="/">Campaign planer</RouterLink>
+        </h3>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+</template>
+
+<style>
+.container {
+    width: 100%;
+    height: 150px;
+}
+</style>
