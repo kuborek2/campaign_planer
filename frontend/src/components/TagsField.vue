11765bb69cfdb2fa6455c292ee8813c2e9b8bd76
Jakub Krezolek <kuborek2@gmail.com>
2022-07-23 21:31:51 +0200

    Added tags with pre-population and typeahead

--- frontend/src/components/TagsField.vue
+++ frontend/src/components/TagsField.vue
@@ -1,36 +1,51 @@
 <template>
   <div class="tags-input-container">
-    <div class="tag" v-for="(tag, index) in tags" :key="'tag'+index">
-      <span >{{ tag }}</span>
-      <span @click="removeTag(index)"><img class="icon" src="../assets/subtract--alt.svg"></span>
+    <div>
+        <div class="tag" v-for="(tag, index) in tags" :key="'tag'+index">
+        <span >{{ tag }}</span>
+        <span @click="removeTag(index)"><img class="icon" src="../assets/subtract--alt.svg"></span>
+        </div>
     </div>
-    <input v-model="tagValue" @keyup.enter="addTag">
+    <!-- <input v-model="tagValue" @keyup.enter="addTag"> -->
+    <SimpleTypeahead
+        placeholder="Start writing..."
+        :items="['One','Two','Three']"
+        :minInputLength="1"
+        ref="tagValue"
+        @selectItem="addTag"
+    >
+    </SimpleTypeahead>
   </div>
+
 </template>
 
 <script setup>
-    import { ref, onUpdated } from 'vue';
+    import { ref } from 'vue';
     import { useTagsStore } from "../stores/tagsStore.js"
+    import SimpleTypeahead from 'vue3-simple-typeahead';
+    import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css'; //Optional default CSS
 
     const tagValue = ref('')
     const tags = ref([])
-    const activeTag = ref(null)
     const tagsStore = useTagsStore();
 
     const addTag = () => {
-        if(!tagValue == '')
-        tags.value.push(tagValue.value);
+        if(!tagValue.value.input == '')
+        tags.value.push(tagValue.value.input);
         tagsStore.change(tags.value);
-        tagValue.value = '';
+        tagValue.value.input = '';
     }
 
     const removeTag = (index) => {
         tags.value.splice(index, 1);
     }
+
 </script>
 
 <style lang="scss" scoped>
   .tags-input-container {
+    display: flex;
+    flex-direction: column;
     width: 75%;
     padding: 10px;
     border: 1px solid black;
